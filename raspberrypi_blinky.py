df66bb2ddbaa9f6aa1f07432f14048a9ff983337
theraginggoblin <43911061+theraginggoblin@users.noreply.github.com>
2019-04-05 16:40:17 +1030

    Update raspberrypi_blinky.py

--- raspberrypi_blinky.py
+++ raspberrypi_blinky.py
@@ -4,6 +4,7 @@ import time
 led_pin = 10
 
 def pin_setup():
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(led_pin, GPIO.OUT)
 
 def led_on():
@@ -12,14 +13,16 @@ def led_on():
 def led_off():
     GPIO.output(led_pin, GPIO.LOW)
 
-def led_blink(milliseconds):
+def led_blink(seconds):
     led_on()
-    time.sleep(milliseconds)
+    time.sleep(seconds)
     led_off()
+
+pin_setup()
 
 try:
     while 1:
-        led_blink(250)
+        led_blink(0.25)
         time.sleep(0.25)
 except KeyboardInterrupt:
     GPIO.cleanup()
